de249f4cc156deb671398bc5c19e7f1705dc1a04
drew08 <anrincongo@unal.edu.co>
2023-05-14 15:42:10 -0500

    Auto-generated commit [ci skip]

--- src/app/components/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './cart/cart.component';
-export * from './footer/footer.component';
-export * from './header/header.component';
-export * from './product/product.component';
--- src/app/modules/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './home/home-routing.module';
-export * from './home/home.component';
-export * from './home/home.module';
