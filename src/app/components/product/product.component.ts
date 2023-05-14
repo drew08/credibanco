04a3a49dcf2f354a87cec82a58b0bbc2476122a6
drew08 <anrincongo@unal.edu.co>
2023-05-13 23:40:15 -0500

    agregar lista de  categorias
    agregar lista de  categorias

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,11 +1,9 @@
-import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CartComponent } from '../cart';
+import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/services';
 import { CartService } from 'src/app/services/cart.service';
 import { FilterPipe } from 'src/app/shared';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NextDirective } from 'src/app/shared/next.directive';
 import { PrevDirective } from 'src/app/shared/prev.directive';
 
@@ -13,64 +11,57 @@ import { PrevDirective } from 'src/app/shared/prev.directive';
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [CommonModule, FilterPipe, NextDirective, PrevDirective,  ],
+  imports: [CommonModule, FilterPipe, NextDirective, PrevDirective,],
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
-  searchKey:string = "";
+  searchKey: string = "";
   public page!: number;
- 
+
   filterPost = '';
-  productList:any;
+  productList: any;
+  categoriesList: any;
   condition = 1;
   variableInterna: any[] = [];
- 
- constructor( private dataService: DataService, private cartService : CartService) { }
- 
-   ngOnInit(): void {
- 
+
+  constructor(private dataService: DataService, private cartService: CartService) { }
+
+  ngOnInit(): void {
     this.getData();
     this.search();
     this.getAllCategories();
- 
-   }
- 
-   search(){
-     this.dataService.search.subscribe((val:any)=>{
-       this.searchKey = val;
-     })
-   }
- 
-   getData(){
+  }
+
+  search() {
+    this.dataService.search.subscribe((val: any) => {
+      this.searchKey = val;
+    })
+  }
+
+  getData() {
     debugger;
-     this.dataService.getData().subscribe((result:any)=>
-     {
-       this.productList = result;
-       console.log(result);
-     });
-     
-   }
+    this.dataService.getData().subscribe((result: any) => {
+      this.productList = result;
+      console.log(result);
+    });
+
+  }
 
 
-   getAllCategories(){
+  getAllCategories() {
     debugger;
-     this.dataService.getAllCategories().subscribe((result:any)=>
-     {
-      debugger;
-       let cat = result;
-       
-       debugger;
-       console.log(result);
-     });
-     
-   }
- 
-   addtocart(item: any){
-     this.cartService.addtoCart(item);
-   }
+    this.dataService.getAllCategories().subscribe((result: any) => {
+      this.categoriesList = result;
+      console.log(result);
+    });
+
+  }
+
+  addtocart(item: any) {
+    this.cartService.addtoCart(item);
+  }
 
 
- 
- }
- 
+
+}
